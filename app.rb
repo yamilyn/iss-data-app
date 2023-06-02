6faf7f2b256e9746bf91e6279b383d6645cfe919
Yamily Benigni <yamilybenigni@Yamilys-Mac-Studio-6.local>
2023-06-02 15:29:10 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# myapp.rb
+require 'sinatra'
+require_relative 'open_notify'
+
+# Allow our templates in views/ to end in `.html.erb`
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
+set :layout, 'views/layout.html.erb'
+
+get '/' do
+  erb :index
+end
+
+get '/position' do
+  iss_now = OpenNotify.iss_now
+
+  erb :position, locals: { data: iss_now }
+end
